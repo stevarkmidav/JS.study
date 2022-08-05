fd841d95ce0d1a237eee6b26212397291dcbaedc
Vadym <stevarkmidav@gmail.com>
2022-08-05 11:54:52 +0300

    Add js study

--- script.js
+++ script.js
@@ -378,8 +378,8 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('Нас колько оцените его?', '');
+    const a = prompt('Один из последних просмотренных фильмов?', '').trim(),
+          b = prompt('Нас колько оцените его?', '');
     
     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
@@ -426,5 +426,246 @@ function writeYourGenres() {
     }
 }
 
-writeYourGenres();
+writeYourGenres(); */
+
+
+
+/* function first() {
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second(); */
+
+
+/* 
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I have done!');
+}
+
+learnJS('JavaScript', done); */
+
+
+
+/* const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+        }
+};
+
+options.makeTest();
+
+const { border, bg } = options.colors;
+console.log(border); */
+
+/* console.log(Object.keys(options).length); */
+
+/* delete options.name;
+
+console.log(options); */
+/* let counter = 0;
+for (let key in options) {
+    if (typeof (options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+}
+console.log(counter); */
+
+/* const arr = [1, 2, 3, 545, 6, 45, 8];
+arr.sort(compareNum);
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+ */
+
+/* arr[99] = 0;
+console.log(arr.length);
+console.log(arr); */
+
+/* arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item} внутри масива ${arr}`);
+}); */
+
+
+/* arr.pop(); */
+/* arr.push(10);
+
+console.log(arr); */
+
+/* for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+} */
+
+/* for (let value of arr) {
+    console.log(value);
+} */
+
+
+/* const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; ')); */
+
+
+/* let a = 5,
+    b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+
+const obj = {
+    a: 5,
+    b: 1
+}; */
+
+/* const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj); */
+
+/* function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10; */
+
+/* console.log(newNumbers);
+console.log(numbers); */
+
+
+/* const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20; */
+
+/* console.log(add);
+console.log(clone); */
+
+/* const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'sdasdasd';
+console.log(newArray);
+console.log(oldArray); */
+
+/* const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+      
+console.log(internet); */
+
+
+/* function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+      
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = { ...q }; */
+
+
+/* const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    const familyClone = family.toString();
+    console.log(`Семья состоит из: ${familyClone}`);
+}
+
+showFamily(); */
+
+
+
+/* const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    let str = '';
+
+    arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+
+    arr.forEach(member => {
+        str += `${member} `;
+    });
+
+    return str;
+}
+
+showFamily(family); */
+
+
+/* const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    arr.forEach(city => {
+        console.log(city.toLowerCase());
+    });
+}
+
+standardizeStrings(favoriteCities);
  */
